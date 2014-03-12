7a7ade596876a423f25a1f89a10401b20e54412e
Steevan BARBOYON <sephirothlune@free.fr>
2014-03-12 21:23:04 +0100

    Add needInstallation and needUpToDate

--- Versions/VersionnedBundle.php
+++ Versions/VersionnedBundle.php
@@ -2,6 +2,7 @@
 namespace kujaff\VersionsBundle\Versions;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use kujaff\VersionsBundle\Versions\Exception;
 
 /**
  * Extends this class instead of Bundle SF2 class to add getVersion
@@ -14,6 +15,44 @@ class VersionnedBundle extends Bundle
 	 * @var Version
 	 */
 	protected $version;
+
+	/**
+	 * Indicate if bundle needs to be installed or if it can be used without installation
+	 *
+	 * @var boolean
+	 */
+	protected $needInstallation = true;
+
+	/**
+	 * Indicate if bundle needs to be up to date or if it can be used without being up to date
+	 *
+	 * @var boolean
+	 */
+	protected $needUpToDate = true;
+
+	/**
+	 * Boot the bundle
+	 *
+	 * @throws Exception
+	 */
+	public function boot()
+	{
+		parent::boot();
+
+		if ($this->needInstallation || $this->needUpToDate) {
+			// if you have a better option to know if we are in console or in normal application ...
+			if (isset($GLOBALS['argv']) && is_array($GLOBALS['argv']) && $GLOBALS['argv'][0] == 'app/console') {
+				return null;
+			}
+			$bundleVersion = $this->container->get('bundle.version')->get($this->getName());
+			if ($this->needInstallation && $bundleVersion->isInstalled() == false) {
+				throw new Exception('Bundle "' . $this->getName() . '" needs to be installed. Exec "php app/console bundle:install ' . $this->getName() . '".');
+			}
+			if ($this->needUpToDate && $bundleVersion->needUpdate()) {
+				throw new Exception('Bundle "' . $this->getName() . '" needs to be updated. Exec "php app/console bundle:update ' . $this->getName() . '".');
+			}
+		}
+	}
 
 	/**
 	 * Return version
