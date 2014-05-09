ea6d76cac1ba7b3a9ff937399960b14f87b2ec46
Steevan BARBOYON <s.barboyon@kreactive.com>
2014-05-09 09:41:14 +0200

    PSR0

--- Command/InstallCommand.php
+++ Command/InstallCommand.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace kujaff\VersionsBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
@@ -6,7 +7,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Input\InputOption;
 
 /**
  * Installation after schema update
@@ -14,50 +14,49 @@ use Symfony\Component\Console\Input\InputOption;
 class InstallCommand extends ContainerAwareCommand
 {
 
-	/**
-	 * {@inherited}
-	 */
-	protected function configure()
-	{
-		$this
-			->setName('bundle:install')
-			->setDescription('Install a bundle')
-			->addArgument('name', InputArgument::REQUIRED)
-			->addOption('force')
-		;
-	}
+    /**
+     * {@inherited}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('bundle:install')
+            ->setDescription('Install a bundle')
+            ->addArgument('name', InputArgument::REQUIRED)
+            ->addOption('force')
+        ;
+    }
 
-	/**
-	 * Run another console command
-	 *
-	 * @param OutputInterface $output
-	 * @param string $command
-	 * @param array $params
-	 */
-	private function _command(OutputInterface $output, $command, $params = array())
-	{
-		$command = $this->getApplication()->find($command);
-		$input = new ArrayInput(array_merge(array($command), $params));
-		$command->run($input, $output);
-	}
+    /**
+     * Run another console command
+     *
+     * @param OutputInterface $output
+     * @param string $command
+     * @param array $params
+     */
+    private function _command(OutputInterface $output, $command, $params = array())
+    {
+        $command = $this->getApplication()->find($command);
+        $input = new ArrayInput(array_merge(array($command), $params));
+        $command->run($input, $output);
+    }
 
-	/**
-	 * {@inherited}
-	 */
-	protected function execute(InputInterface $input, OutputInterface $output)
-	{
-		$name = $input->getArgument('name');
+    /**
+     * {@inherited}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('name');
 
-		// install
-		$output->write('[<comment>' . $name . '</comment>] Installing ... ');
-		$installedVersion = $this->getContainer()->get('bundle.installer')->install($name, $input->getOption('force'));
-		$output->writeln('<info>' . $installedVersion->asString() . '</info> installed.');
+        // install
+        $output->write('[<comment>' . $name . '</comment>] Installing ... ');
+        $installedVersion = $this->getContainer()->get('bundle.installer')->install($name, $input->getOption('force'));
+        $output->writeln('<info>' . $installedVersion->asString() . '</info> installed.');
 
-		// update
-		$bundleVersion = $this->getContainer()->get('bundle.version')->getBundleVersion($name);
-		if ($installedVersion->asString() != $bundleVersion->getVersion()->asString()) {
-			$this->_command($output, 'bundle:update', array('name' => $name));
-		}
-	}
-
+        // update
+        $bundleVersion = $this->getContainer()->get('bundle.version')->getBundleVersion($name);
+        if ($installedVersion->asString() != $bundleVersion->getVersion()->asString()) {
+            $this->_command($output, 'bundle:update', array('name' => $name));
+        }
+    }
 }
