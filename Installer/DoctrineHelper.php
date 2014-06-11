5f45c08e008f8266a1fd08023dd2c9b856f2529d
Steevan BARBOYON <s.barboyon@kreactive.com>
2014-06-11 10:03:49 +0200

    Commentaire pour l'auto complétion

--- Installer/DoctrineHelper.php
+++ Installer/DoctrineHelper.php
@@ -1,5 +1,8 @@
 <?php
+
 namespace kujaff\VersionsBundle\Installer;
+
+use Doctrine\DBAL\Statement;
 
 /**
  * Helps your using Doctrine
@@ -8,50 +11,50 @@ namespace kujaff\VersionsBundle\Installer;
 trait DoctrineHelper
 {
 
-	/**
-	 * Execute a DQL query (only for SELECT, UPDATE or DELETE)
-	 *
-	 * @param string $dql
-	 * @param array $parameters
-	 * @return mixed
-	 */
-	protected function _executeDQL($dql, array $parameters = array())
-	{
-		$em = $this->container->get('doctrine')->getManager();
-		$query = $em->createQuery($dql);
-		foreach ($parameters as $name => $value) {
-			$query->setParameter($name, $value);
-		}
-		return $query->getResult();
-	}
+    /**
+     * Execute a DQL query (only for SELECT, UPDATE or DELETE)
+     *
+     * @param string $dql
+     * @param array $parameters
+     * @return mixed
+     */
+    protected function _executeDQL($dql, array $parameters = array())
+    {
+        $em = $this->container->get('doctrine')->getManager();
+        $query = $em->createQuery($dql);
+        foreach ($parameters as $name => $value) {
+            $query->setParameter($name, $value);
+        }
+        return $query->getResult();
+    }
 
-	/**
-	 * Execute raw SQL
-	 *
-	 * @param string $sql
-	 * @param array $parameters
-	 */
-	protected function _executeSQL($sql, array $parameters = array())
-	{
-		$em = $this->container->get('doctrine')->getManager();
-		$stmt = $em->getConnection()->prepare($sql);
-		foreach ($parameters as $name => $value) {
-			$stmt->bindValue($name, $value);
-		}
-		$stmt->execute();
-		return $stmt;
-	}
+    /**
+     * Execute raw SQL
+     *
+     * @param string $sql
+     * @param array $parameters
+     * @return Statement
+     */
+    protected function _executeSQL($sql, array $parameters = array())
+    {
+        $em = $this->container->get('doctrine')->getManager();
+        $stmt = $em->getConnection()->prepare($sql);
+        foreach ($parameters as $name => $value) {
+            $stmt->bindValue($name, $value);
+        }
+        $stmt->execute();
+        return $stmt;
+    }
 
-	/**
-	 * Drop tables if exists
-	 *
-	 * @param array $tables
-	 */
-	protected function _dropTables(array $tables)
-	{
-		foreach ($tables as $table) {
-			$this->_executeSQL('DROP TABLE IF EXISTS ' . $table);
-		}
-	}
-
+    /**
+     * Drop tables if exists
+     *
+     * @param array $tables
+     */
+    protected function _dropTables(array $tables)
+    {
+        foreach ($tables as $table) {
+            $this->_executeSQL('DROP TABLE IF EXISTS ' . $table);
+        }
+    }
 }
